921b410b39e0fca1e02ef249f0f64bd1edb811ac
Marcella Park <marcellajpark@gmail.com>
2017-03-22 18:44:52 -0400

    optimized matrixmult caching, adapted output to spec

--- strassen.c
+++ strassen.c
@@ -17,6 +17,7 @@ int** makematrix(int n){
     return matrix;
 }
 
+/*
 void matrixmult(int n, int** A, int** B, int** C){
     int rowtrav;
     int coltrav;
@@ -32,10 +33,25 @@ void matrixmult(int n, int** A, int** B, int** C){
         }
     }
 }
+*/
+
+void matrixmult(int n, int** A, int** B, int** C){
+    int rowtravA;
+    int rowtravB;
+    int coltravB;
+    for (rowtravA = 0; rowtravA < n; rowtravA++){
+        C[rowtravA][coltravB] = 0;
+        for (rowtravB = 0; rowtravB < n; rowtravB++){
+            for (coltravB = 0; coltravB < n; coltravB ++){
+                C[rowtravA][coltravB] = C[rowtravA][coltravB] + A[rowtravA][rowtravB] * B[rowtravB][coltravB];
+            }
+        }
+    }
+}
 
 int** matrixadd(int n, int** A, int** B){
     int **sum = makematrix(n);
-    
+
     int rowtrav;
     int coltrav;
     for (rowtrav = 0; rowtrav < n; rowtrav++){
@@ -48,7 +64,7 @@ int** matrixadd(int n, int** A, int** B){
 
 int** matrixsubtract(int n, int** A, int** B){
     int** diff = makematrix(n);
-    
+
     int rowtrav;
     int coltrav;
     for (rowtrav = 0; rowtrav < n; rowtrav++){
@@ -131,7 +147,7 @@ void split4(int d, int pad, int** m, int** m4){
 void join(int d, int pad, int** m, int** m1, int** m2, int** m3, int** m4){
     int n;
     int rows, cols;
-    
+
     if (pad == 0){
         n = 2*d;
         for (rows = 0; rows < d; rows++){
@@ -168,43 +184,43 @@ int** strassen(int n, int** A, int** B){
     int** C = makematrix(n);
     int pad;
     int d;
-    
+
     // cutoff
     if (n <= 32){
         matrixmult(n, A, B, C);
         return C;
     }
-    
+
     if (n % 2 == 1) {
         d = (n+1)/2;
         pad = 1;
     }
-    
+
     if (n % 2 == 0){
         d = n/2;
         pad = 0;
     }
-    
+
     int** A1 = makematrix(d);
     int** A2 = makematrix(d);
     int** A3 = makematrix(d);
     int** A4 = makematrix(d);
-    
+
     int** B1 = makematrix(d);
     int** B2 = makematrix(d);
     int** B3 = makematrix(d);
     int** B4 = makematrix(d);
-    
+
     split1(d, A, A1);
     split2(d, pad, A, A2);
     split3(d, pad, A, A3);
     split4(d, pad, A, A4);
-    
+
     split1(d, B, B1);
     split2(d, pad, B, B2);
     split3(d, pad, B, B3);
     split4(d, pad, B, B4);
-    
+
     int** p1 = makematrix(d);
     int** p2 = makematrix(d);
     int** p3 = makematrix(d);
@@ -212,7 +228,7 @@ int** strassen(int n, int** A, int** B){
     int** p5 = makematrix(d);
     int** p6 = makematrix(d);
     int** p7 = makematrix(d);
-    
+
     p1 = strassen(d, A1, matrixsubtract(d, B2, B4));
     p2 = strassen(d, matrixadd(d, A1, A2), B4);
     p3 = strassen(d, matrixadd(d, A3, A4), B1);
@@ -220,19 +236,19 @@ int** strassen(int n, int** A, int** B){
     p5 = strassen(d, matrixadd(d, A1, A4), matrixadd(d, B1, B4));
     p6 = strassen(d, matrixsubtract(d, A2, A4), matrixadd(d, B3, B4));
     p7 = strassen(d, matrixsubtract(d, A1, A3), matrixadd(d, B1, B2));
-    
+
     int** C1 = makematrix(d);
     int** C2 = makematrix(d);
     int** C3 = makematrix(d);
     int** C4 = makematrix(d);
-    
+
     C1 = matrixadd(d, p5, matrixadd(d, p6, matrixsubtract(d, p4, p2)));
     C2 = matrixadd(d, p1, p2);
     C3 = matrixadd(d, p3, p4);
     C4 = matrixsubtract(d, matrixadd(d, p5, p1), matrixadd(d, p3, p7));
-    
+
     join(d, pad, C, C1, C2, C3, C4);
-    
+
     free(p1);
     free(p2);
     free(p3);
@@ -240,63 +256,63 @@ int** strassen(int n, int** A, int** B){
     free(p5);
     free(p6);
     free(p7);
-    
+
     free(A1);
     free(A2);
     free(A3);
     free(A4);
-    
+
     free(B1);
     free(B2);
     free(B3);
     free(B4);
-    
+
     free(C1);
     free(C2);
     free(C3);
     free(C4);
-    
+
     return C;
 }
 
 
 int main(int argc, char *argv[]){
     int n = atoi(argv[2]);
-    
+
     FILE *nums = fopen(argv[3], "r");
     if (nums == NULL){
         fprintf(stderr, "Could not open file");
         exit(1);
     }
-    
+
     char line[100];
     int length = 0;
     while (fgets(line, sizeof(line), nums) != NULL){
         length++;
     }
-    
+
     fseek(nums, 0, SEEK_SET);
-    
+
     int matgen[length];
     int trav = 0;
-    
+
     while (fgets(line, sizeof(line), nums) != NULL){
         int new = atoi(line);
         matgen[trav] = new;
         trav++;
     }
-    
+
     // Matrices are arrays of rows
     int** matrixA = makematrix(n);
     int** matrixB = makematrix(n);
-    
+
     // seed for random index generator for generating random matrix from list
     time_t seconds;
     time(&seconds);
     srand((unsigned int) seconds);
-    
+
     int randindex = 0;
-    
+
     for (int rows = 0; rows < n; rows ++){
         for (int cols = 0; cols < n; cols++){
             randindex = rand() % length;
@@ -305,40 +321,50 @@ int main(int argc, char *argv[]){
             matrixB[rows][cols] = matgen[randindex];
         }
     }
-    
+
     int** C = makematrix(n);
-    
+
     clock_t start, finish;
     double strasstime;
     double conventime;
-    
+
     start = clock();
     C = strassen(n, matrixA, matrixB);
     finish = clock();
-    
+
     strasstime = (double)(finish - start) / CLOCKS_PER_SEC;
-    
+
     start = clock();
     matrixmult(n, matrixA, matrixB, C);
     finish = clock();
-    
+
     conventime = (double)(finish - start) / CLOCKS_PER_SEC;
-    
-    printf("dim \t strassen \t conventional \n");
-    for (int dim = 1; dim <= n; dim *= 2){
-        start = clock();
-        C = strassen(dim, matrixA, matrixB);
-        finish = clock();
-        strasstime = (double)(finish - start) / CLOCKS_PER_SEC;
-        
-        start = clock();
-        matrixmult(dim, matrixA, matrixB, C);
-        finish = clock();
-        conventime = (double)(finish - start) / CLOCKS_PER_SEC;
-        
-        printf("%i \t %f \t %f \n", dim, strasstime, conventime);
+
+    // testing
+    if (atoi(argv[1]) == 1){
+        printf("dim \t strassen \t conventional \n");
+        for (int dim = 1; dim <= n; dim *= 2){
+            start = clock();
+            C = strassen(dim, matrixA, matrixB);
+            finish = clock();
+            strasstime = (double)(finish - start) / CLOCKS_PER_SEC;
+
+            start = clock();
+            matrixmult(dim, matrixA, matrixB, C);
+            finish = clock();
+            conventime = (double)(finish - start) / CLOCKS_PER_SEC;
+
+            printf("%i \t %f \t %f \n", dim, strasstime, conventime);
+      }
     }
-    
+    // print diagonal entries with newlines
+    else {
+        for (int i = 0; i < n; i ++) {
+            int val = C[i][i];
+            printf("%i\n", val);
+        }
+    }
+
     free(matrixA);
     free(matrixB);
 }
